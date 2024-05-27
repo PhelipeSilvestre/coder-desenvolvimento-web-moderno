713abbfb2909d06eb287daa1d30272c55ba42b2d
Phelipe Silvestre <phelipe.silvestre@ICOM.Local>
2024-05-27 13:56:41 -0300

    add: desafios 01 e 02

---
+++ modulos/01.fundamentos/desafios/01.sentenca_bloco.js
@@ -0,0 +1,9 @@
+// Forneça um codigo com dois blocos de sentenças
+
+{
+    console.log("essa é o primeiro bloco");
+}
+
+{
+    console.log("essa é o segundo bloco");
+}
---
+++ modulos/01.fundamentos/desafios/02.area_circunferencia.js
@@ -0,0 +1,9 @@
+// Calcule a area da circunferencia usando o PI = 3.14159
+
+const PI = 3.14159;
+
+const raio = 5.6;
+
+const area = PI * raio * raio;
+
+console.log(area);
