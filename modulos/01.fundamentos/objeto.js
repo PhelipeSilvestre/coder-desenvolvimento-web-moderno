0a7b131e12662784a8567d7cf0f970021d0e1d3f
phelipeSilvestre <99892687+PhelipeSilvestre@users.noreply.github.com>
2024-09-13 12:36:49 -0300

    Update objeto.js

--- modulos/01.fundamentos/objeto.js
+++ modulos/01.fundamentos/objeto.js
@@ -1,13 +1,18 @@
+// Objetos em Js são um conjunto de chave - valor
 const prod1 = {}
+
+// é possivel adicionar atributos fora do objeto
 prod1.nome = 'Celular Ultra Mega'
 prod1.preco = 4998.90
 prod1['Desconto Legal'] = 0.40 // evitar atributos com espaço
 
 console.log(prod1)
 
+// É possivel adicionar objetos dentro de objetos
 const prod2 = {
     nome: 'Camisa Polo',
-    preco: 79.90
+    preco: 79.90,
+    obj3: {blabla: 1}
 }
 
 console.log(prod2)
